14faac652bdc739c813a81385b540972cd411c21
Samir Satam <ssatam@gmail.com>
2021-12-31 22:49:55 -0500

    Playing with Slices.

---
+++ slices.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	aSlice := []float64{}
+	// cap() function - capacity of the slice, cap doubles each time length reaches the cap size.
+	fmt.Println(aSlice, len(aSlice), cap(aSlice))
+
+	aSlice = append(aSlice, 1234.56)
+	aSlice = append(aSlice, -34.0)
+	fmt.Println(aSlice, "with length", len(aSlice))
+
+	// A slice with length 4
+	t := make([]int, 4)
+	t[0] = -1
+	t[1] = -2
+	t[2] = -3
+	t[3] = -4
+
+	t = append(t, -5)
+	fmt.Println(t)
+
+	twoD := [][]int{{1, 2, 3}, {4, 5, 6}}
+
+	for _, i := range twoD {
+		for _, k := range i {
+			fmt.Print(k, " ")
+		}
+		fmt.Println()
+	}
+
+	make2D := make([][]int, 2)
+	fmt.Println(make2D)
+	make2D[0] = []int{1, 2, 3, 4}
+	make2D[1] = []int{-1, -2, -3, -4}
+	fmt.Println(make2D)
+}
